daf95fc0519d568505b4e49930854eaaf9fcb614
Kaitlyn Lee <kaitlynlee19@gmail.com>
2015-02-21 15:42:14 -0800

    Adding counter view

---
+++ iOS/TeamMakk/TeamMakk/CounterView.m
@@ -0,0 +1,32 @@
+//
+//  CounterView.m
+//  TeamMakk
+//
+//  Created by Kaitlyn Lee on 2/21/15.
+//  Copyright (c) 2015 teammakk. All rights reserved.
+//
+
+#import "CounterView.h"
+
+@implementation CounterView
+- (id)initWithFrame:(CGRect)frame
+{
+  self = [super initWithFrame:frame];
+  if (self) {
+    [self setUpView];
+  }
+  return self;
+}
+
+- (void)setUpView {
+  
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
